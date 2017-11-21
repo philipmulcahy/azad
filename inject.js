c34c3e090a1b936cdc39d529e789aa66b723a11f
Philip Mulcahy <azadextension@gmail.com>
2017-11-21 17:27:12 +0000

    privatised lots of inject.YearFetcher functions

--- inject.js
+++ inject.js
@@ -39,6 +39,95 @@ var amazon_order_history_inject = (function() {
                     "&unifiedOrders=0"
             };
             this.orderPromises = [];
+			this.sendGetOrderCount = function() {
+				request_scheduler.schedule(
+					this.generateQueryString(0),
+					this.receiveGetOrderCount.bind(this),
+					"00000"
+				);
+			};
+			this.generateQueryString = function(startOrderPos) {
+				var template = this.query_string_templates[amazon_order_history_util.getSite()];
+				return sprintf(
+					template,
+					{
+						site: amazon_order_history_util.getSite(),
+						year: this.year,
+						startOrderPos: startOrderPos
+					}
+				);
+			};
+			this.receiveGetOrderCount = function(evt) {
+				var iorder;
+				var p = new DOMParser();
+				var d = p.parseFromString(evt.target.responseText, "text/html");
+				var countSpan = amazon_order_history_util.findSingleNodeValue(
+					".//span[@class=\"num-orders\"]", d, d);
+				this.expected_order_count = parseInt(
+					countSpan.textContent.split(" ")[0], 10);
+				amazon_order_history_util.updateStatus(
+					"Found " + this.expected_order_count + " orders for " + this.year
+				);
+				this.unfetched_count = this.expected_order_count;
+				if(isNaN(this.unfetched_count)) {
+					amazon_order_history_util.updateStatus(
+						"Error: cannot find order count in " + countSpan.textContent
+					);
+					this.unfetched_count = 0;
+				}
+				// Request second and subsequent pages.
+				for(iorder = 10; iorder < this.expected_order_count; iorder += 10) {
+					request_scheduler.schedule(
+						this.generateQueryString(iorder),
+						this.receiveOrdersPage.bind(this),
+						"2"
+					);
+				}
+				// We already have the first page.
+				this.receiveOrdersPage(evt);
+			};
+			this.receiveOrdersPage = function(evt) {
+				var p = new DOMParser();
+				var d = p.parseFromString(evt.target.responseText, "text/html");
+				var orders;
+				var ordersElem;
+				var elem;
+				var i;
+				try {
+					ordersElem = d.getElementById("ordersContainer");
+				} catch(err) {
+					amazon_order_history_util.updateStatus(
+						"Error: maybe you\"re not logged into " +
+						"https://" + amazon_order_history_util.getSite() + "/gp/css/order-history " +
+						err
+					);
+					return;
+				}
+				orders = amazon_order_history_util.findMultipleNodeValues(
+					".//*[contains(concat(\" \", " +
+						"normalize-space(@class), " +
+						"\" \"), " +
+						"\" order \")]",
+					d,
+					ordersElem
+				);
+				function makeOrderPromise(elem) {
+					return new Promise(
+						function(resolve, reject) {
+							resolve(
+								amazon_order_history_order.create(elem, request_scheduler)
+							);
+						}
+					);
+				}
+				orders.forEach(
+					function(elem){
+						this.order_found_callback(
+							makeOrderPromise(elem)
+						);
+					}.bind(this)
+				); 
+			};
 
             /* Promise to array of Order Promise. */
             this.ordersPromise = new Promise(
@@ -65,130 +154,37 @@ var amazon_order_history_inject = (function() {
                 }.bind(this)
             );
         }
-
-        generateQueryString(startOrderPos) {
-            var template = this.query_string_templates[amazon_order_history_util.getSite()];
-            return sprintf(
-                template,
-                {
-                    site: amazon_order_history_util.getSite(),
-                    year: this.year,
-                    startOrderPos: startOrderPos
-                }
-            );
-        }
-
-        sendGetOrderCount() {
-            request_scheduler.schedule(
-                this.generateQueryString(0),
-                this.receiveGetOrderCount.bind(this),
-                "00000"
-            );
-        }
-
-        receiveGetOrderCount(evt) {
-            var iorder;
-            var p = new DOMParser();
-            var d = p.parseFromString(evt.target.responseText, "text/html");
-            var countSpan = amazon_order_history_util.findSingleNodeValue(
-                ".//span[@class=\"num-orders\"]", d, d);
-            this.expected_order_count = parseInt(
-                countSpan.textContent.split(" ")[0], 10);
-            amazon_order_history_util.updateStatus(
-                "Found " + this.expected_order_count + " orders for " + this.year
-            );
-            this.unfetched_count = this.expected_order_count;
-            if(isNaN(this.unfetched_count)) {
-                amazon_order_history_util.updateStatus(
-                    "Error: cannot find order count in " + countSpan.textContent
-                );
-                this.unfetched_count = 0;
-            }
-            // Request second and subsequent pages.
-            for(iorder = 10; iorder < this.expected_order_count; iorder += 10) {
-                request_scheduler.schedule(
-                    this.generateQueryString(iorder),
-                    this.receiveOrdersPage.bind(this),
-                    "2"
-                );
-            }
-            // We already have the first page.
-            this.receiveOrdersPage(evt);
-        }
-
-        receiveOrdersPage(evt) {
-            var p = new DOMParser();
-            var d = p.parseFromString(evt.target.responseText, "text/html");
-            var orders;
-            var ordersElem;
-            var elem;
-            var i;
-            try {
-                ordersElem = d.getElementById("ordersContainer");
-            } catch(err) {
-                amazon_order_history_util.updateStatus(
-                    "Error: maybe you\"re not logged into " +
-                    "https://" + amazon_order_history_util.getSite() + "/gp/css/order-history " +
-                    err
-                );
-                return;
-            }
-            orders = amazon_order_history_util.findMultipleNodeValues(
-                ".//*[contains(concat(\" \", " +
-                    "normalize-space(@class), " +
-                    "\" \"), " +
-                    "\" order \")]",
-                d,
-                ordersElem
-            );
-            function makeOrderPromise(elem) {
-                return new Promise(
-                    function(resolve, reject) {
-                        resolve(
-                            amazon_order_history_order.create(elem, request_scheduler)
-                        );
-                    }
-                );
-            }
-            orders.forEach(
-                function(elem){
-                    this.order_found_callback(
-                        makeOrderPromise(elem)
-                    );
-                }.bind(this)
-            ); 
-        }
     }
 
-    function getYears() {
-        if(typeof(getYears.years) === "undefined") {
-            var snapshot = amazon_order_history_util.findMultipleNodeValues(
-                "//select[@name=\"orderFilter\"]/option[@value]",
-                document,
-                document);
-            getYears.years = snapshot.map(
-                function(elem){
-                    return elem.textContent.trim();
-                }
-            ).filter(
-                function(element, index, array) {
-                    return(/^\d+$/).test(element);
-                }
-            );
-        }
-        return getYears.years;
-    }
+	/* Returns array of Promise to array of Order Promises. */
+	function getOrdersByYear(years) {
+		// At return time we may not know how many orders there are, only
+		// how many years in which orders have been queried for.
+		return years.map(
+			function(year) {
+				return new YearFetcher(year).ordersPromise;
+			}
+		);
+	}
 
-    /* Returns array of Promise to array of Order Promises. */
-    function getOrdersByYear(years) {
-        // At return time we may not know how many orders there are, only
-        // how many years in which orders have been queried for.
-        return years.map(
-            function(year) {
-                return new YearFetcher(year).ordersPromise;
-            }
-        );
-    }
+	function getYears(){
+		if(typeof(getYears.years) === "undefined") {
+			var snapshot = amazon_order_history_util.findMultipleNodeValues(
+				"//select[@name=\"orderFilter\"]/option[@value]",
+				document,
+				document);
+			getYears.years = snapshot.map(
+				function(elem){
+					return elem.textContent.trim();
+				}
+			).filter(
+				function(element, index, array) {
+					return(/^\d+$/).test(element);
+				}
+			);
+		}
+		return getYears.years;
+	}
 
     function fetchAndShowOrders(years) {
         var orders = getOrdersByYear(years);
