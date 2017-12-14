71464bada8ef8d0a943fcd930c330afdbaca67c9
Philip Mulcahy <azadextension@gmail.com>
2017-12-13 21:50:58 -0800

    add amazon.it - guessed URL pattern

--- order.js
+++ order.js
@@ -279,6 +279,13 @@ var amazon_order_history_order = (function() {
                     "&orderFilter=year-%(year)s" +
                     "&startIndex=%(startOrderPos)s" +
                     "&language=en_GB",
+                "www.amazon.it": "https://%(site)s/gp/css/order-history" +
+                    "?opt=ab&digitalOrders=1" +
+                    "&unifiedOrders=1" +
+                    "&returnTo=" +
+                    "&orderFilter=year-%(year)s" +
+                    "&startIndex=%(startOrderPos)s" +
+                    "&language=en_GB",
                 "smile.amazon.com": "https://%(site)s/gp/css/order-history" +
                     "?opt=ab&digitalOrders=1&" +
                     "&unifiedOrders=1" +
