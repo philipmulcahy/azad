868a4cce12af41b747d42997660b67e545c9f78c
philip <philip@mulcahyfamily.org>
2018-05-17 22:51:40 +0100

    datatables and jquery un-minified and upgraded in preparation for possible firefox port

--- inject.js
+++ inject.js
@@ -2,71 +2,66 @@
 /* global window */
 /* jshint strict: true, esversion: 6 */
 
-var amazon_order_history_inject = (function() {
-    "use strict";
+const amazon_order_history_inject = (function() {
+    'use strict';
 
-    var request_scheduler = amazon_order_history_request_scheduler.create();
+    const request_scheduler = amazon_order_history_request_scheduler.create();
 
 	function getYears(){
-		if(typeof(getYears.years) === "undefined") {
-			var snapshot = amazon_order_history_util.findMultipleNodeValues(
-				"//select[@name=\"orderFilter\"]/option[@value]",
+		if(typeof(getYears.years) === 'undefined') {
+			const snapshot = amazon_order_history_util.findMultipleNodeValues(
+				'//select[@name=\"orderFilter\"]/option[@value]',
 				document,
 				document);
-			getYears.years = snapshot.map(
-				function(elem){
-					return elem.textContent
-                               .replace("nel", "")  // amazon.it
-                               .trim();
-				}
-			).filter(
-				function(element, index, array) {
-					return(/^\d+$/).test(element);
-				}
-			);
+			getYears.years = snapshot.map( elem => {
+                return elem.textContent
+                           .replace('nel', '')  // amazon.it
+                           .trim();
+            }).filter( (element, index, array) => {
+                return(/^\d+$/).test(element);
+            });
 		}
 		return getYears.years;
 	}
 
     function fetchAndShowOrders(years) {
-		return amazon_order_history_order.getOrdersByYear(
+		amazon_order_history_order.getOrdersByYear(
             years, request_scheduler
         ).then(
-			function(orderPromises) {
+			orderPromises => {
 				amazon_order_history_table.displayOrders(orderPromises, true);
-				return document.querySelector("[id=\"order_table\"]");
+				return document.querySelector('[id=\"order_table\"]');
 			}
 		);
     }
 
     function addYearButtons() {
-        var years = getYears();
+        const years = getYears();
+        chrome.runtime.sendMessage({action: 'open_new_tab'});
         if(years.length > 0) {
             amazon_order_history_util.addButton(
-                "All years",
-                function() {
+                'All years',
+                () => {
                     fetchAndShowOrders(years);
                 }
             );
         }
-        years.forEach(
-            function(year) {
-                amazon_order_history_util.addButton(
-                    [year],
-                    function() {
-                        fetchAndShowOrders([year]);
-                    }
-                );
-            }
-        );
+        years.forEach( year => {
+            amazon_order_history_util.addButton(
+                [year],
+                () => {
+                    fetchAndShowOrders([year]);
+                }
+            );
+        });
     }
 
     function addInfoPoints() {
-        var progress = document.createElement("div");
-        progress.setAttribute("id", "order_reporter_progress");
-        progress.setAttribute("class", "order_reporter_progress");
+        const progress = document.createElement('div');
+        progress.setAttribute('id', 'order_reporter_progress');
+        progress.setAttribute('class', 'order_reporter_progress');
         progress.setAttribute(
-            "style", "position:absolute; top:0; right:0; color:orange; padding:0.2em; font-size:75%");
+            'style', 'position:absolute; top:0; right:0; color:orange; padding:0.2em; font-size:75%');
         document.body.insertBefore(
             progress,
             document.body.firstChild
@@ -75,5 +70,5 @@ var amazon_order_history_inject = (function() {
 
     addYearButtons();
     addInfoPoints();
-    console.log("Starting");
+    console.log('Starting');
 })();
