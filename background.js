53d216c4e3b1541a422c4bc47901c5c731290e37
Philip Mulcahy <azadextension@gmail.com>
2018-05-18 23:27:21 +0100

    surface login prompt on a new tab after warning dialog

---
+++ background.js
@@ -0,0 +1,15 @@
+/* jshint strict: true, esversion: 6 */
+
+function registerListener() {
+    "use strict";
+    chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+        console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension');
+        if (request.action == 'open_tab') {
+            chrome.tabs.create(
+                { url: request.url }
+            );
+        }
+    });
+}
+
+registerListener();
