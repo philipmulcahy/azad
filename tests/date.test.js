93726ece0f8bc47be1a5f3db1c2753f545a17b18
Philip Mulcahy <azadextension@gmail.com>
2018-07-18 00:28:48 +0100

    making progress on parsing

--- tests/date.test.js
+++ tests/date.test.js
@@ -8,8 +8,17 @@ const date_tests = (() => {
          return date.normalizeDateString('29 mai 2018') == '2018-05-29';
     };
 
+    const generic_test = () => {
+         return (
+             date.normalizeDateString('October 14, 2016') == '2016-10-14' && // US
+             date.normalizeDateString('15 July 2018') == '2018-07-15' &&     // UK
+             date.normalizeDateString('29 mai 2018') == '2018-05-29'         // FR
+         );
+    };
+
     return {
-        fr_locale_test: fr_locale_test
+        fr_locale_test: fr_locale_test,
+        generic_test: generic_test
     };
 })()
 
