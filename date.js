05c6040422bc97bf302d08508d3cf477f7a16cb6
Dana Norwood <nimadera@nimadera.com>
2018-09-23 15:28:04 -0400

    Normalize date using locale-aware long date format

--- date.js
+++ date.js
@@ -9,26 +9,12 @@ const date = (() => {
         return sprintf('%d-%02d-%02d', d.getYear()+1900, d.getMonth()+1, d.getDate());
     }
 
-    const FORMATS = [
-        {format: 'DD MMM YYYY', locale: 'fr'},
-        {format: 'D MMM YYYY', locale: 'fr'},
-        {format: 'DD. MMM YYYY', locale: 'fr'},
-        {format: 'D. MMM YYYY', locale: 'fr'},
-        {format: 'MMMM DD, YYYY', locale: 'en'},
-        {format: 'DD MMMM YYYY', locale: 'en'},
-        {format: 'D MMMM YYYY', locale: 'en'},
-        {format: 'DD MMMM YYYY', locale: 'de'},
-        {format: 'D MMMM YYYY', locale: 'de'},
-        {format: 'DD. MMMM YYYY', locale: 'de'},
-        {format: 'D. MMMM YYYY', locale: 'de'},
-        {format: 'DD MMMM YYYY', locale: 'it'},
-        {format: 'D MMMM YYYY', locale: 'it'},
-        {format: 'DD. MMMM YYYY', locale: 'it'},
-        {format: 'D. MMMM YYYY', locale: 'it'}
-    ];
+    const LOCALES = ['de', 'en', 'en-gb', 'fr', 'it'];
+
     function normalizeDateString(ds) {
-        const mom = FORMATS.map( rule => moment(ds, rule.format, rule.locale, true) )
+        const mom = LOCALES.map( locale => moment(ds, 'LL', locale, true) )
                            .filter( m => m.isValid() )[0];
+
         if (!mom) {
             console.warn('could not parse date: ' + ds);
             return ds;
