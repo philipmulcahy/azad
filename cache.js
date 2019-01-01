03660147b6adf26d906f6d57b95b0b17552b4fa2
philip <philip@mulcahyfamily.org>
2019-01-01 21:28:01 +0000

    cache basic tests working

--- cache.js
+++ cache.js
@@ -34,11 +34,15 @@ var cachestuff = (function(){
 
     function trim() {
         const keys = Object.keys(window.localStorage);
-        timestamps_by_key = {};
+        const timestamps_by_key = {};
         keys.forEach( key => {
-            timestamps_by_key[key] = JSON.parse(window.localStorage.getItem(key)).timestamp;
+            try {
+                timestamps_by_key[key] = JSON.parse(window.localStorage.getItem(key)).timestamp;
+            } catch(error) {
+                console.debug('couldn\'t get timestamp for key: ' + key);
+            }
         });
-        timestamps = Object.values(timestamps_by_key);
+        const timestamps = Object.values(timestamps_by_key);
         timestamps.sort();
         const cutoff_timestamp = timestamps[Math.floor(keys.length * 0.1)];
         Object.keys(timestamps_by_key).forEach( key => {
