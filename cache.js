6d51f7fa86d4cf7e700b22bcfd040beab7f62a81
philip <philip@mulcahyfamily.org>
2019-01-12 17:16:47 +0000

    cache isolation passes tests, but isn't integrated to azad

--- cache.js
+++ cache.js
@@ -4,80 +4,102 @@
 var cachestuff = (function(){
     "use strict";
 
-    TODO: make it a class, and have named caches - tests are mullering us!
-
     function millisNow() {
         return (new Date()).getTime();
     }
 
-    function reallySet(real_key, value) {
-        window.localStorage.setItem(
-            real_key,
-            JSON.stringify({
-                timestamp: millisNow(),
-                value: value
-            })
-        );
-    }
 
-    function set(key, value) {
-        const real_key = 'AZAD_' + key;
-        try {
-            reallySet(real_key, value);
-        } catch(error) {
-            console.log('failed to set ' + key + ': ' + error);
-            trim();
-            reallySet(real_key, value);
-            console.log('set ' + key + ' on second attempt after trimming cache');
+    class CacheImpl {
+        constructor(cache_name) {
+            this.cache_name = cache_name;
+            this.key_stem = 'AZAD_' + this.cache_name + '_';
         }
-    }
 
-    function get(key) {
-        const real_key = 'AZAD_' + key;
-        try {
-            const encoded = window.localStorage.getItem(real_key);
-            const packed = JSON.parse(encoded);
-            return packed.value;
-        } catch(err) {
-//            return undefined;
-            return 'XXX';
+        buildRealKey(key) {
+            return this.key_stem + key;
         }
-    }
 
-    function getKeys() {
-        return Object.keys(window.localStorage).filter(
-            key => key.startsWith('AZAD_')
-        );
-    }
+        reallySet(real_key, value) {
+            window.localStorage.setItem(
+                real_key,
+                JSON.stringify({
+                    timestamp: millisNow(),
+                    value: value
+                })
+            );
+        }
 
-    function entry_count() {
-        return getKeys().length;
-    }
-
-    function trim() {
-        console.log('trimming cache');
-        const keys = getKeys();
-        const timestamps_by_key = {};
-        keys.forEach( key => {
+        set(key, value) {
+            const real_key = this.buildRealKey(key);
             try {
-                timestamps_by_key[key] = JSON.parse(window.localStorage.getItem(key)).timestamp;
+                this.reallySet(real_key, value);
             } catch(error) {
-                console.debug('couldn\'t get timestamp for key: ' + key);
+                console.log('failed to set ' + key + ': ' + error);
+                this.trim();
+                this.reallySet(real_key, value);
+                console.log('set ' + key + ' on second attempt after trimming cache');
             }
-        });
-        const timestamps = Object.values(timestamps_by_key);
-        timestamps.sort();
-        const cutoff_timestamp = timestamps[Math.floor(keys.length * 0.1)];
-        Object.keys(timestamps_by_key).forEach( key => {
-            if (timestamps_by_key[key] <= cutoff_timestamp) {
-                window.localStorage.removeItem(key);        
+        }
+
+        get(key) {
+            const real_key = this.buildRealKey(key);
+            try {
+                const encoded = window.localStorage.getItem(real_key);
+                const packed = JSON.parse(encoded);
+                return packed.value;
+            } catch(err) {
+    //            TODO return undefined;
+                return 'XXX';
             }
-        });
+        }
+
+        getRealKeys() {
+            return Object.keys(window.localStorage).filter(
+                key => key.startsWith(this.key_stem)
+            );
+        }
+
+        trim() {
+            console.log('trimming cache');
+            const real_keys = this.getRealKeys();
+            const timestamps_by_key = {};
+            real_keys.forEach( key => {
+                try {
+                    timestamps_by_key[key] = JSON.parse(window.localStorage.getItem(key)).timestamp;
+                } catch(error) {
+                    console.debug('couldn\'t get timestamp for key: ' + key);
+                }
+            });
+            const timestamps = Object.values(timestamps_by_key);
+            timestamps.sort();
+            const cutoff_timestamp = timestamps[Math.floor(real_keys.length * 0.1)];
+            Object.keys(timestamps_by_key).forEach( key => {
+                if (timestamps_by_key[key] <= cutoff_timestamp) {
+                    window.localStorage.removeItem(key);        
+                }
+            });
+        }
+
+        clear() {
+            console.log('clearing cache');
+            this.getRealKeys().forEach( key => {
+                window.localStorage.removeItem(key);
+            });
+        }
+    }
+
+    
+    const createCache = (cache_name) => {
+        const cache = new CacheImpl(cache_name);
+        return {
+            set: (key, value) => cache.set(key, value),
+            get: key => cache.get(key),
+            clear: () => cache.clear(),
+            getEntryCount: cache.getRealKeys().length
+        }
     }
 
     return {
-        set: set,
-        get: get,
-        entry_count: entry_count
+        createCache: createCache
     };
 })();
