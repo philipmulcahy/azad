0add5223c4c1d067cb3aaac8dc57bc629e691c40
philip <philip@mulcahyfamily.org>
2019-09-29 21:33:05 +0100

    experiments with popup and right click menu

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,21 @@
 /* Copyright(c) 2018 Philip Mulcahy. */
 /* jshint strict: true, esversion: 6 */
 
+
+function registerPopups() {
+    chrome.contextMenus.create(
+        {
+            id: 'save_order_debug_info',
+            title: 'save order debug info',
+            contexts: ['link']
+//            onclick: () => console.log('save order debug info clicked')
+        }
+    );
+    chrome.contextMenus.onClicked.addListener(info => {
+        console.log('context menu item: ' + info.menuItemId + ' clicked;');
+    });
+}
+
 function registerListener() {
     "use strict";
     chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
@@ -17,4 +32,5 @@ function registerListener() {
     });
 }
 
+registerPopups();
 registerListener();
