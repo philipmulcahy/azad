79dcf1c92892d6610281f6d998413c74f4d624ad
philip <philip@mulcahyfamily.org>
2019-10-08 22:59:53 +0100

    making progress on mocked up popup ui

--- src/js/control.js
+++ src/js/control.js
@@ -1,7 +1,28 @@
-/* Copyright(c) 2018 Philip Mulcahy. */
-/* Copyright(c) 2016 Philip Mulcahy. */
+/* Copyright(c) 2019 Philip Mulcahy. */
 
 /* jshint strict: true, esversion: 6 */
+
+'use strict';
+
+import $ from 'jquery';
+
+function activateIdle() {
+    $('#azad_clear_cache').removeClass('hidden');
+    $('#azad_stop').addClass('hidden');
+    $('#azad_show_simple').addClass('hidden');
+    $('#azad_show_fancy').addClass('hidden');
+    $('#azad_hide_controls').removeClass('hidden');
+    console.log('hello world');
+}
+
+function activateScraping(years) {
+
+}
+
+function activateYearsDone(years) {
+}
+
+$(document).ready( activateIdle );
 
 export default {
 };
