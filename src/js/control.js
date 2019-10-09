a566a1ef791212be9de5e098cae6ba41409f9b2a
Philip Mulcahy <azadextension@gmail.com>
2019-10-09 15:40:48 +0100

    years populating in popup

--- src/js/control.js
+++ src/js/control.js
@@ -7,22 +7,69 @@
 import $ from 'jquery';
 
 function activateIdle() {
-    $('#azad_clear_cache').removeClass('hidden');
-    $('#azad_stop').addClass('hidden');
-    $('#azad_show_simple').addClass('hidden');
-    $('#azad_show_fancy').addClass('hidden');
-    $('#azad_hide_controls').removeClass('hidden');
+    console.log('activateIdle');
+    showOnly(['azad_clear_cache', 'azad_hide_controls']);
     console.log('hello world');
 }
 
 function activateScraping(years) {
-
+    console.log('activateScraping');
+    showOnly(['azad_stop', 'azad_hide_controls']);
 }
 
-function activateYearsDone(years) {
+function activateDone(years) {
+    console.log('activateDone');
+    showOnly(['azad_clear_cache', 'azad_hide_controls']);
 }
 
-$(document).ready( activateIdle );
+function showOnly(button_ids) {
+    $('.azad_action').addClass('hidden');
+    button_ids.forEach( id => $('#' + id).removeClass('hidden') ); 
+}
 
-export default {
-};
+function connectToBackground() {
+    console.log('connectToBackground');
+    const background_port = chrome.runtime.connect(null, { name: 'azad_control' });
+    background_port.onMessage.addListener( msg => {
+        switch(msg.action) {
+            case 'scrape_complete':
+                break;
+            case 'advertise_years':
+                showYearButtons(msg.years);
+                break;
+            default:
+                console.warn('unknown action: ' + msg.action); 
+        }
+    });
+}
+
+function registerActionButtons() {
+}
+
+function stop() {
+}
+
+function hide() {
+}
+
+function start(years) {
+}
+
+function showYearButtons(years) {
+    console.log('show year buttons', years);
+    $('.azad_year_button').remove();
+    years.sort().reverse().forEach( year => {
+        $('#azad_year_list').append(
+            '<input type="button" class="azad_year_button" value="' + year + '" />'
+        );
+    });
+}
+
+function init() {
+    console.log('init');
+    activateIdle();
+    connectToBackground();
+    registerActionButtons();
+}
+
+$(document).ready( () => init() );
