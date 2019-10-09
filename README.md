51f29e5834184af78fcb2e4fb88125921715eab0
ScottMcNay <ScottMcNay@users.noreply.github.com>
2019-10-08 20:14:09 -0500

    Add documentation and support for building and packaging with Windows.

--- README.md
+++ README.md
@@ -4,7 +4,26 @@
 ### official installation page
 https://chrome.google.com/webstore/detail/amazon-order-history-repo/mgkilgclilajckgnedgjgnfdokkgnibi
 
+### supported development package(s)
+Node.js®, available at https://nodejs.org
+
+### supported development platforms
+MacOS, Windows
+
+#### MacOS
+
+#### Windows install
+* Install Node.js® from https://nodejs.org. Enable the option to install Chocolatey, Python 2, and Visual Studio Build Tools.
+* Install GitHub Desktop from https://desktop.github.com.
+* Open GitHub Desktop, go to File, Clone Repository, clone from philipmulcahy/azad.
+* Start CMD
+* 	CD %UserProfile%\Documents\GitHub\azad
+(This is the path shown by GitHub Desktop)
+* 	npm install --init
+
+
 ### linting (uses npm lint tools)
+You'll need to build first, the first time.
 ```
 npm run lint
 ```
@@ -14,14 +33,19 @@ npm run lint
 npm run build
 ```
 
-### packaging
+### packaging; this includes building
+#### for MacOS
 ```
 npm run package
+```
+#### for Windows
+```
+npm run winpackage
 ```
 
 ### installing locally on chrome
 * Open chrome, and type chrome://extensions in the address bar.
-* Click "Load unpacked extension...".
+* Click "Load unpacked".
 * Navigate to the build folder.
 
 ### running 'unit' tests
@@ -29,4 +53,4 @@ npm run package
 * open/refresh src/tests/tests.html in your browser
 
 ### edit-test-loop
-Remember to re-build, reload the extension on the chrome://extensions page and reload the targetted amazon page after saving changes to files.
+Remember to re-build, reload the extension on the chrome://extensions page and reload the targeted amazon page after saving changes to files.
---
+++ utils/winpackage.cmd
@@ -0,0 +1,59 @@
+@echo off
+:: This is for packaging under windows.
+:: Currently requires Powershell 6 or newer, which is standard with Windows 10. If you need to support older versions of Windows, use the vbs version included below.
+
+:: We want variables to disappear when cmd file exits
+setlocal
+
+:: Set variables. Use single-quotes if names contain spaces.
+set Source=build
+set ZipFile=azad.zip
+
+:: Go to parent of folder containing this cmd.
+cd "%~dp0.."
+:: run npm command
+cmd /C npm run %Source%
+
+:: Delete old package
+del %ZipFile%
+:: Create new package. Windows does not havr a commandline zip command, so tis is the alternate. Must have Powershell installed.
+powershell -ExecutionPolicy ByPass -Command Compress-Archive %Source% %ZipFile%
+
+echo.
+echo To test changes, go to chrome://extensions, click "Load unpacked", and navigate to %CD%\%Source%.
+echo.
+echo If you are the extension owner, go to https://chrome.google.com/webstore/developer/edit/mgkilgclilajckgnedgjgnfdokkgnibi and upload the zip file.
+
+endlocal
+:: End of execcution.
+goto :EOF
+
+:: The text below is skipped.
+
+:: Use this instead of powershell for older versions of Windows; supposedly will support back to Windows 98 or Internet Explorer 5.
+:: Note that this version has a kludgy time delay.
+
+cscript utils\zipcmd.vbs %Source% %ZipFile%
+
+
+:: zipcmd.vbs follows:
+
+// This is a "poor man's zip utility", since Windows doesn't have a built-in zip command-line utility.
+// The oldest source of this which I could find, dated 2006-05-17, is https://www.tek-tips.com/viewthread.cfm?qid=1231429
+// 2011-11-19 modern appearance: http://www.block-net.de/Programmierung/scripts/scripts.html
+// 2011-07-04: http://qtpknowledgesharing.blogspot.com/2011/07/function-to-create-zip-file-of-folder.html
+// (2010): https://superuser.com/questions/110991/can-you-zip-a-file-from-the-command-prompt-using-only-windows-built-in-capabili
+
+Set objArgs = WScript.Arguments
+Set FS = CreateObject("Scripting.FileSystemObject")
+InputFolder = FS.GetAbsolutePathName(objArgs(0))
+ZipFile = FS.GetAbsolutePathName(objArgs(1))
+
+CreateObject("Scripting.FileSystemObject").CreateTextFile(ZipFile, True).Write "PK" & Chr(5) & Chr(6) & String(18, vbNullChar)
+
+Set objShell = CreateObject("Shell.Application")
+Set source = objShell.NameSpace(InputFolder).Items
+
+objShell.NameSpace(ZipFile).CopyHere(source)
+wScript.Sleep 3000
+
