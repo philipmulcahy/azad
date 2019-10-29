168252f09d083bb28ef4bb3e9df9b9c44a95e653
ScottMcNay <ScottMcNay@users.noreply.github.com>
2019-10-28 22:39:34 -0500

    XPathResult already defined

--- src/js/util.js
+++ src/js/util.js
@@ -1,7 +1,7 @@
 /* Copyright(c) 2018 Philip Mulcahy. */
 /* Copyright(c) 2016 Philip Mulcahy. */
 
-/* global XPathResult */
+///global_XPathResult///
 
 /* jshint strict: true, esversion: 6 */
 /* jslint node:true */
