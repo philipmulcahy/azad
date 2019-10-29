c674f38a0993a2a830c3ac98a5ae53cd537e4c09
ScottMcNay <ScottMcNay@users.noreply.github.com>
2019-10-28 20:03:08 -0500

    x

--- utils/build.js
+++
@@ -1,9 +0,0 @@
-const webpack = require("webpack");
-const config = require("../webpack.config");
-
-delete config.chromeExtensionBoilerplate;
-
-webpack(
-    config,
-    function (err) { if (err) throw err; }
-);
