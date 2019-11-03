d6efb7bbd4d26a73e74b98133e5f07a630df6a84
ScottMcNay <ScottMcNay@users.noreply.github.com>
2019-11-03 04:09:30 -0600

    20191029 to (#78)
    * Fix 'to' for US
    
    * var --> let

--- src/js/order.js
+++ src/js/order.js
@@ -23,6 +23,25 @@ function getField(xpath, elem) {
 }
 
 function extractDetailFromDoc(order, doc) {
+
+    const who = function(){
+        let x = getField('//table[contains(@class,"sample")]/tbody/tr/td/div/text()[2]', doc.documentElement); // US Digital
+        if ( !x ) {
+            x = getField('.//div[contains(@class,"recipient")]' +
+                '//span[@class="trigger-text"]', doc.documentElement);
+            if ( !x ) {
+                x = getField('.//div[contains(text(),"Recipient")]', doc.documentElement);
+                if ( !x ) {
+                    x = getField('//li[contains(@class,"displayAddressFullName")]/text()', doc.documentElement);
+                    if ( !x ) {
+                        x = 'n/a';
+                    }
+                }
+            }
+        }
+        return x;
+    };
+
     const order_date = function(){
         return date.normalizeDateString(
             extraction.by_regex(
@@ -264,7 +283,8 @@ function extractDetailFromDoc(order, doc) {
         vat: vat(),
         gst: cad_gst(),
         pst: cad_pst(),
-        refund: refund()
+        refund: refund(),
+        who: who(),
     };
 }
 
