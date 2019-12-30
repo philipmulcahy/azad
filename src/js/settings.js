c3b0e6e7630ce8c4163dbef194f1b73eaa24c857
philip <philip@mulcahyfamily.org>
2019-12-30 21:59:48 +0000

    vue bit seems to work

--- src/js/settings.js
+++ src/js/settings.js
@@ -5,26 +5,13 @@
 
 import Vue from 'vue';
 
-const options = [
-    {
-        id: 'show_where_are_my_buttons',
-        text: 'show "where are my buttons" help at top of amazon pages"',
-        default: true,
-    },
-    {
-        id: 'show_where_are_my_other_buttons',
-        text: 'show "where are my other buttons" help at top of amazon pages"',
-        default: false,
-    },
-];
-
 function initialiseUi() {
     const vue_settings_app = new Vue({
-        el: '#azad_settings_row',
-        data: {
-            items: options,
-        },
-    });
+      el: '#azad_settings',
+      data: {
+        checked_settings: []
+      }
+    })
 }
 
 
