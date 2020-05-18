9a45ca6547aa0d29ec56a3879189d5c5ca7f1c9f
Philip Mulcahy <azadextension@gmail.com>
2020-05-18 22:34:09 +0100

    explained moment shennanigans

--- src/js/date.ts
+++ src/js/date.ts
@@ -4,6 +4,9 @@
 
 "use strict";
 
+// 2020-05 moment's typing is not 'normal'.
+// See https://stackoverflow.com/questions/36648231/how-can-moment-js-be-imported-with-typescript
+// and also esModuleInterop = true in tsconfig.js 
 const moment = require('moment');
 
 import { sprintf } from 'sprintf-js';
