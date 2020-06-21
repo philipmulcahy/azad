2decd2e7c289f297fcb9c897192b51cc861d2b89
Philip Mulcahy <azadextension@gmail.com>
2020-06-21 16:07:59 +0100

    fixed harder

--- src/js/settings.ts
+++ src/js/settings.ts
@@ -3,34 +3,12 @@
 
 "use strict";
 
-const Vue = require('vue');
-
 export function initialiseUi() {
     const KEY = 'azad_settings';
-    const vue_settings_app = new Vue({
-        el: '#azad_settings',
-        data: {
-            checked_settings: []
-        },
-        watch: {
-            checked_settings: function(new_settings: any) {
-                const value = JSON.stringify(new_settings);
-                const entry: Record<string, any> = {};
-                entry[KEY] = value;
-                chrome.storage.sync.set(
-                    entry,
-                    function() {
-                        console.log('settings stored: ' + value);
-                    }
-                );
-            }
-        }
-    });
     chrome.storage.sync.get(
         KEY,
         function(entries) {
             console.log('settings retrieved: ' + JSON.stringify(entries));
-            vue_settings_app.checked_settings = JSON.parse(entries[KEY]);
         }
     );
 }
