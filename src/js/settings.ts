b7c5cdf15a197fa6785eaa70b628bb6d6ddbbac6
Philip Mulcahy <azadextension@gmail.com>
2020-06-27 22:35:46 +0100

    csv total row controlled by persisted setting instead of button choice

--- src/js/settings.ts
+++ src/js/settings.ts
@@ -5,57 +5,68 @@ import * as util from './util';
 
 "use strict";
 
-export function initialiseUi() {
-    const KEY = 'azad_settings';
-    chrome.storage.sync.get(
-        KEY,
-        function(entries) {
-            console.log('retrieved settings entries', entries);
-            const settings = JSON.parse(entries[KEY]);
-            console.log('translated settings entries', settings);
-            console.log('settings retrieved: ' + JSON.stringify(settings));
-            const key_to_elem: Record<string, HTMLElement> = {};
-            util.findMultipleNodeValues(
-                '//div[@id="azad_settings"]/input',
-                document.documentElement
-            ).map( node => <HTMLElement>node ).forEach( elem => {
-                const key = elem.getAttribute('id');
-                key_to_elem[key] = elem;
-            });
-            for ( let key in key_to_elem ) {
-                let value: boolean = <boolean>settings[key];
-                const elem = key_to_elem[key];
-                if (value) {
-                    elem.setAttribute('checked', 'true');
-                }
-                elem.onclick = () => {
-                    value = value ? false : true;
-                    settings[key] = value;
-                    console.log('settings updated', settings);
-                    const stringified_settings =  JSON.stringify(settings);
-                    console.log('settings stringified', stringified_settings);
-                    const updated_entries: Record<string, string> = {};
-                    updated_entries[KEY] = stringified_settings;
-                    chrome.storage.sync.set(
-                        updated_entries,
-                        function() {
-                            if (chrome.runtime.lastError) {
-                                console.warn(
-                                    'settings not written:',
-                                    chrome.runtime.lastError.message
-                                );
-                            } else {
-                                console.log('settings written');
-                            }
-                        }
-                    )
+const KEY = 'azad_settings';
+
+export function initialiseUi(): Promise<void> {
+    return new Promise<void>( function( resolve ) {
+        chrome.storage.sync.get(
+            KEY,
+            function(entries) {
+                const settings = JSON.parse(entries[KEY]);
+                const key_to_elem: Record<string, HTMLElement> = {};
+                util.findMultipleNodeValues(
+                    '//div[@id="azad_settings"]/input',
+                    document.documentElement
+                ).map( node => <HTMLElement>node ).forEach( elem => {
+                    const key = elem.getAttribute('id');
+                    key_to_elem[key] = elem;
+                });
+                for ( let key in key_to_elem ) {
+                    let value: boolean = <boolean>settings[key];
+                    const elem = key_to_elem[key];
                     if (value) {
                         elem.setAttribute('checked', 'true');
-                    } else {
-                        elem.removeAttribute('checked');
                     }
+                    elem.onclick = () => {
+                        value = value ? false : true;
+                        settings[key] = value;
+                        const stringified_settings =  JSON.stringify(settings);
+                        const updated_entries: Record<string, string> = {};
+                        updated_entries[KEY] = stringified_settings;
+                        chrome.storage.sync.set(
+                            updated_entries,
+                            function() {
+                                if (chrome.runtime.lastError) {
+                                    console.warn(
+                                        'settings not written:',
+                                        chrome.runtime.lastError.message
+                                    );
+                                }
+                            }
+                        )
+                        if (value) {
+                            elem.setAttribute('checked', 'true');
+                        } else {
+                            elem.removeAttribute('checked');
+                        }
+                    };
                 };
+                resolve();
             }
-        }
-    );
+        );
+    });
+}
+
+export function getBoolean(key: string): Promise<boolean> {
+    return new Promise<boolean>( function( resolve ) {
+        chrome.storage.sync.get(
+            KEY,
+            function(entries) {
+                const settings = JSON.parse(entries[KEY]);
+                const key_to_elem: Record<string, HTMLElement> = {};
+                const value: boolean = <boolean>settings[key];
+                resolve(value);
+            }
+        );
+    });
 }
