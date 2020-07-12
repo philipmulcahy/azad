4db8dca2ff678abb13b95f1bb02193f1f1cdec98
philip <philip@mulcahyfamily.org>
2020-07-12 08:50:06 +0100

    invoice link visibility controlled by setting

--- src/js/table.ts
+++ src/js/table.ts
@@ -45,7 +45,7 @@ const TAX_HELP = 'Caution: tax is often not listed when stuff is not supplied by
 
 type RenderFunc = (order: azad_order.IOrder, td: HTMLElement) => Promise<void>;
 
-const cols: Record<string, any>[] = [
+const COLS: Record<string, any>[] = [
     {
         field_name: 'order id',
         render_func:
@@ -189,11 +189,30 @@ const cols: Record<string, any>[] = [
             });
         },
         is_numeric: false,
+        visibility: () => settings.getBoolean('show_invoice_links')
     }
-].filter( col => ('sites' in col) ?
-    col.sites.test(util.getSite()):
-    true
-);
+]
+
+function getCols(): Promise< Record<string, any>[] > {
+    const waits: Promise<any>[] = [];
+    const results: Record<string, any>[] = [];  
+    COLS.forEach( col => {
+        if ( ('sites' in col) ? col.sites.test(util.getSite()) : true ) {
+            if ( 'visibility' in col ) {
+                const visible_promise: Promise<boolean> = col.visibility();
+                waits.push(visible_promise);
+                visible_promise.then( visible => {
+                    if ( visible ) {
+                        results.push( col );
+                    }
+                });
+            } else {
+                results.push( col );
+            }
+        }
+    });
+    return Promise.all(waits).then( _ => results );
+}
 
 function appendCell(
     tr: HTMLTableRowElement,
@@ -261,10 +280,12 @@ function appendCell(
 function appendOrderRow(
     table: HTMLElement,
     order: azad_order.IOrder
-): Promise<void>[] {
+): Promise<Promise<void>[]> {
     const tr = document.createElement('tr');
     table.appendChild(tr);
-    return cols.map( col_spec => appendCell(tr, order, col_spec) );
+    return getCols().then( cols =>
+        cols.map( col_spec => appendCell(tr, order, col_spec) )
+    );
 }
 
 function addOrderTable(
@@ -316,35 +337,34 @@ function addOrderTable(
     fr.setAttribute('id', 'azad_order_table_fr');
     tfoot.appendChild(fr);
 
-    cols.forEach( col_spec => {
-        addHeader(hr, col_spec.field_name, col_spec.help);
-        addHeader(fr, col_spec.field_name, col_spec.help);
-    });
-
-    const tbody = doc.createElement('tbody');
-    table.appendChild(tbody);
-
-    // Record all the promises: we're going to need to wait on all of them to
-    // resolve before we can hand over the table to our callers.
-    const row_done_promises: Promise<Promise<void>[]>[] = orders.map(
-        order => order.id().then(
-            id => appendOrderRow(tbody, order)
-        )
-    );
-
-    const table_promise = Promise.resolve(table);
-    if (wait_for_all_values_before_resolving) {
-        return Promise.all(row_done_promises).then( row_promises => {
-            const value_done_promises: Promise<void>[] = [];
-            row_promises.forEach(
-                cell_done_promises => value_done_promises.push(...cell_done_promises)
-            )
-            return Promise.all(value_done_promises).then( _ => table_promise );
+    return getCols().then( cols => {
+        cols.forEach( col_spec => {
+            addHeader(hr, col_spec.field_name, col_spec.help);
+            addHeader(fr, col_spec.field_name, col_spec.help);
         });
 
-    } else {
-        return table_promise;
-    }
+        const tbody = doc.createElement('tbody');
+        table.appendChild(tbody);
+
+        // Record all the promises: we're going to need to wait on all of them to
+        // resolve before we can hand over the table to our callers.
+        const row_done_promises = orders.map(
+            order => appendOrderRow(tbody, order)
+        );
+
+        if (wait_for_all_values_before_resolving) {
+            return Promise.all(row_done_promises).then( row_promises => {
+                const value_done_promises: Promise<void>[] = [];
+                row_promises.forEach(
+                    cell_done_promises => value_done_promises.push(...cell_done_promises)
+                )
+                return Promise.all(value_done_promises).then( _ => table );
+            });
+
+        } else {
+            return table;
+        }
+    });
 }
 
 function reallyDisplayOrders(
@@ -397,7 +417,7 @@ function reallyDisplayOrders(
                             return candidate;
                         };
                         let col_index = 0;
-                        cols.forEach( col_spec => {
+                        getCols().then( cols => cols.forEach( col_spec => {
                             if(col_spec.is_numeric) {
                                 col_spec.sum = floatVal(
                                     api.column(col_index)
@@ -418,7 +438,7 @@ function reallyDisplayOrders(
                                 );
                             }
                             col_index += 1;
-                        });
+                        }));
                     }
                 });
                 util.removeButton('data table');
