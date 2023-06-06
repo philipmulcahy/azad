aeefa80d76058ad11bd461f0a186d467781e1681
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2023-06-06 02:57:08 +0100

    made preview features more visible - faded when disabled, and with popup alerts for unpaid users

--- src/js/control.ts
+++ src/js/control.ts
@@ -4,6 +4,7 @@
 
 const $ = require('jquery');
 
+import * as ui_messages from  './ui_messages';
 import * as settings from './settings';
 import * as util from './util';
 
@@ -19,25 +20,25 @@ $(document).ready(function() {
 });
 
 function activateIdle(): void {
-    console.log('activateIdle');
-    actionsShowOnly(['azad_clear_cache', 'azad_force_logout', 'azad_hide_controls']);
+  console.log('activateIdle');
+  actionsShowOnly(['azad_clear_cache', 'azad_force_logout', 'azad_hide_controls']);
 }
 
 function activateScraping(years: number[]): void {
-    console.log('activateScraping');
-    actionsShowOnly(['azad_stop', 'azad_hide_controls']);
-    $('#azad_state').text('scraping ' + years.join(','));
+  console.log('activateScraping');
+  actionsShowOnly(['azad_stop', 'azad_hide_controls']);
+  $('#azad_state').text('scraping ' + years.join(','));
 }
 
 function activateDone(periods: number): void {
-    console.log('activateDone');
-    actionsShowOnly(['azad_clear_cache', 'azad_force_logout', 'azad_hide_controls']);
-    $('#azad_state').text(periods);
+  console.log('activateDone');
+  actionsShowOnly(['azad_clear_cache', 'azad_force_logout', 'azad_hide_controls']);
+  $('#azad_state').text(periods);
 }
 
 function actionsShowOnly(button_ids: string[]): void {
-    $('.azad_action').addClass('hidden');
-    button_ids.forEach( id => $('#' + id).removeClass('hidden') );
+  $('.azad_action').addClass('hidden');
+  button_ids.forEach( id => $('#' + id).removeClass('hidden') );
 }
 
 function showNormalPage(): void {
@@ -45,56 +46,61 @@ function showNormalPage(): void {
 }
 
 function showExtensionPay(): void {
-    pagesShowOnly(['azad_page_actions', 'azad_page_extensionpay']);
+  pagesShowOnly(['azad_page_actions', 'azad_page_extensionpay']);
 }
 
 function pagesShowOnly(button_ids: string[]): void {
-    $('.azad_control_page').addClass('hidden');
-    button_ids.forEach( id => $('#' + id).removeClass('hidden') );
+  $('.azad_control_page').addClass('hidden');
+  button_ids.forEach( id => $('#' + id).removeClass('hidden') );
 }
 
 let background_port: chrome.runtime.Port|null = null;
 function connectToBackground() {
-    console.log('connectToBackground');
+  console.log('connectToBackground');
 
-    // @ts-ignore: tsc objects to null first parameter for connect();
-    background_port = chrome.runtime.connect(null, { name: 'azad_control' });
+  // @ts-ignore: tsc objects to null first parameter for connect();
+  background_port = chrome.runtime.connect(null, { name: 'azad_control' });
 
-    background_port.onMessage.addListener( msg => {
-        switch(msg.action) {
-            case 'advertise_periods':
-                console.info('control got periods advertisement');
-                const months = (msg.periods as number[]).filter(p => p<=12);
-                const years = (msg.periods as number[]).filter(p => p>=2000);
-                showMonthsButtons(months);
-                showYearButtons(years);
-                break;
-            case 'statistics_update':
-                console.info('control got statistics update');
-                {
-                    const text = Object.entries(msg.statistics)
-                        .map(([k,v]) => {return k + ':' + v;})
-                        .join('; ');
-                    $('#azad_statistics').text(text);
-                    if ((msg.statistics.queued + msg.statistics.running) > 0) {
-                        activateScraping(msg.purpose);
-                    } else {
-                        activateDone(msg.purpose);
-                    }
-                }
-                break;
-            case 'authorisation_status':
-                console.info('control got authorisation_status message');
-                const authorised = msg.authorisation_status;
-                handleAuthorisationMessage(authorised);
-                break;
-            default:
-                console.warn('unknown action: ' + msg.action);
+  background_port.onMessage.addListener( msg => {
+    switch(msg.action) {
+      case 'advertise_periods':
+        console.info('control got periods advertisement');
+      const periods = msg.periods;
+      handleAdvertisePeriods(periods);
+      break;
+      case 'statistics_update':
+        console.info('control got statistics update');
+      {
+        const text = Object.entries(msg.statistics)
+        .map(([k,v]) => {return k + ':' + v;})
+        .join('; ');
+        $('#azad_statistics').text(text);
+        if ((msg.statistics.queued + msg.statistics.running) > 0) {
+          activateScraping(msg.purpose);
+        } else {
+          activateDone(msg.purpose);
         }
-    });
+      }
+      break;
+      case 'authorisation_status':
+        console.info('control got authorisation_status message');
+      const authorised = msg.authorisation_status;
+      handleAuthorisationMessage(authorised);
+      break;
+      default:
+        console.warn('unknown action: ' + msg.action);
+    }
+  });
 
-    background_port.postMessage(
-      {action: 'check_feature_authorized', feature_id: 'premium_preview'})
+  background_port.postMessage(
+    {action: 'check_feature_authorized', feature_id: 'premium_preview'})
+}
+
+async function handleAdvertisePeriods(periods: number[]): Promise<void> {
+  const months = periods.filter(p => p<=12);
+  const years = periods.filter(p => p>=2000);
+  await showMonthsButtons(months);
+  showYearButtons(years);
 }
 
 function handleAuthorisationMessage(authorised: boolean): void {
@@ -105,146 +111,152 @@ function handleAuthorisationMessage(authorised: boolean): void {
 }
 
 function registerActionButtons() {
-    $('#azad_clear_cache').on('click', () => {
-      if (background_port) {
-        console.log('clear cache clicked');
-        background_port.postMessage({action: 'clear_cache'});
-      } else {
-        console.warn('clear cache clicked, but I have no background port');
-      }
-    });
-    $('#azad_force_logout').on('click', () => {
+  $('#azad_clear_cache').on('click', () => {
+    if (background_port) {
+      console.log('clear cache clicked');
+      background_port.postMessage({action: 'clear_cache'});
+    } else {
+      console.warn('clear cache clicked, but I have no background port');
+    }
+  });
+  $('#azad_force_logout').on('click', () => {
+    console.log('force logout clicked');
+    if (background_port) {
       console.log('force logout clicked');
-      if (background_port) {
-        console.log('force logout clicked');
-        background_port.postMessage({action: 'force_logout'});
-      } else {
-        console.warn('force logout clicked, but I have no background port');
-      }
-    });
-    $('#azad_stop').on('click', () => {
-      console.log('stop clicked');
-      handleStopClick();
-    });
-    $('#azad_hide_controls').on('click', () => {
-      console.log('closing popup');
-      window.close();
-    });
-    $('#azad_payment_ui_button').on('click', () => {
-      if (background_port) {
-        console.log('show payment UI clicked')
-        background_port.postMessage({action: 'show_payment_ui'});
-      } else {
-        console.warn('show payment UI clicked, but I have no background port');
-      }
-    });
-    $('#azad_extpay_login_button').on('click', () => {
-      if (background_port) {
-        console.log('show payment UI clicked')
-        background_port.postMessage({action: 'show_extpay_login_ui'});
-      } else {
-        console.warn('show extpay log-in UI clicked, but I have no background port');
-      }
-    });
+      background_port.postMessage({action: 'force_logout'});
+    } else {
+      console.warn('force logout clicked, but I have no background port');
+    }
+  });
+  $('#azad_stop').on('click', () => {
+    console.log('stop clicked');
+    handleStopClick();
+  });
+  $('#azad_hide_controls').on('click', () => {
+    console.log('closing popup');
+    window.close();
+  });
+  $('#azad_payment_ui_button').on('click', () => {
+    if (background_port) {
+      console.log('show payment UI clicked')
+      background_port.postMessage({action: 'show_payment_ui'});
+    } else {
+      console.warn('show payment UI clicked, but I have no background port');
+    }
+  });
+  $('#azad_extpay_login_button').on('click', () => {
+    if (background_port) {
+      console.log('show payment UI clicked')
+      background_port.postMessage({action: 'show_extpay_login_ui'});
+    } else {
+      console.warn('show extpay log-in UI clicked, but I have no background port');
+    }
+  });
 }
 
 function registerPageButtons(): void {
-    showNormalPage();
-    $('#azad_switch_extensionpay').on('click', () => {
-      showExtensionPay();
-    });
+  showNormalPage();
+  $('#azad_switch_extensionpay').on('click', () => {
+    showExtensionPay();
+  });
 
-    $('#azad_switch_default').on('click', () => {
-      showNormalPage();
-    });
+  $('#azad_switch_default').on('click', () => {
+    showNormalPage();
+  });
 }
 
 function showYearButtons(years: number[]) {
-    console.log('show year buttons', years);
-    $('.azad_year_button').remove();
-    years.sort().reverse().forEach( year => {
-        $('#azad_year_list').append(
-            '<button class="azad_year_button" value="' + year + '">' + year + '</button>'
-        );
-    });
-    $('.azad_year_button').on('click', handleYearClick);
-}
-
-function showMonthsButtons(month_counts: number[]) {
-  console.log('show month buttons', month_counts);
-  $('.azad_months_button').remove();
-  month_counts.sort().forEach( month_count => {
+  console.log('show year buttons', years);
+  $('.azad_year_button').remove();
+  years.sort().reverse().forEach( year => {
     $('#azad_year_list').append(
-      '<button class="azad_months_button" value="' + month_count + '" >' + month_count + 'm</button>' 
+      '<button class="azad_year_button" value="' + year + '">' + year + '</button>'
     );
   });
+  $('.azad_year_button').on('click', handleYearClick);
+}
+
+async function showMonthsButtons(month_counts: number[]): Promise<void> {
+  console.log('show month buttons', month_counts);
+  $('.azad_months_button').remove();
+  const authorised = await settings.getBoolean('preview_features_enabled');
+  console.log('showMonthButtons(...) authorised = ' + authorised);
+  const classes = 'azad_months_button' + (authorised ? '' : ' azad_disabled');
+  console.log('showMonthButtons(...) classes = ' + classes);
+  month_counts.sort().forEach( month_count => {
+    $('#azad_year_list').append(
+      '<button class="' + classes + '" value="' + month_count + '" >' +
+      month_count + 'm</button>' 
+    );
+  });
+  console.log('showMonthButtons(...) buttons placed');
   $('.azad_months_button').on('click', handleMonthsClick);
+  console.log('showMonthButtons(...) buttons wired up');
 }
 
 function handleYearClick(evt: { target: { value: any; }; }) {
-    const year = evt.target.value;
-    const years = [year];
-    activateScraping(years);
-    if (background_port) {
-        console.log('sending scrape_years', year);
-        background_port.postMessage({
-            action: 'scrape_years',
-            years: years,
-        });
-    } else {
-        console.warn('background_port not set');
-    }
+  const year = evt.target.value;
+  const years = [year];
+  activateScraping(years);
+  if (background_port) {
+    console.log('sending scrape_years', year);
+    background_port.postMessage({
+      action: 'scrape_years',
+      years: years,
+    });
+  } else {
+    console.warn('background_port not set');
+  }
+}
+
+async function checkFeaturesAuthorisedNoAlert(): Promise<boolean> {
+	const authorised = await settings.getBoolean('preview_features_enabled');
+	return authorised;
 }
 
 async function checkFeatureAuthorised(): Promise<boolean> {
-	const authorised = await settings.getBoolean('preview_features_enabled');
+  const authorised = await checkFeaturesAuthorisedNoAlert();
   if (!authorised) {
-		alert(
-			'This capability is part of a preview feature set that will be ' +
-			'released when charity donations reach a target set out in ' +
-			'https://github.com/philipmulcahy/azad/blob/master/doc/commercial_features.md ' +
-			'but you get early access by subscribing to the preview channel: ' +
-			'click on the "preview features" button to find out more.'
-		);
+		alert(ui_messages.preview_feature_disabled);
 	};
 	return authorised;
 }
 
 async function handleMonthsClick(evt: { target: { value: any; }; }) {
-		const authorised = await checkFeatureAuthorised(); 
-	  if (!authorised) {
-			return;
-		}
-    const month_count = Number(evt.target.value);
-    const end_date = new Date();
-    const start_date = util.subtract_months(end_date, month_count);
-    activateScraping([month_count]);
-    if (background_port) {
-        console.log('sending scrape_range', start_date, end_date);
-        background_port.postMessage({
-            action: 'scrape_range',
-            start_date: start_date,
-            end_date: end_date,
-        });
-    } else {
-        console.warn('background_port not set');
-    }
+  const authorised = await checkFeatureAuthorised(); 
+  if (!authorised) {
+    return;
+  }
+  const month_count = Number(evt.target.value);
+  const end_date = new Date();
+  const start_date = util.subtract_months(end_date, month_count);
+  activateScraping([month_count]);
+  if (background_port) {
+    console.log('sending scrape_range', start_date, end_date);
+    background_port.postMessage({
+      action: 'scrape_range',
+      start_date: start_date,
+      end_date: end_date,
+    });
+  } else {
+    console.warn('background_port not set');
+  }
 }
 
 function handleStopClick() {
-    if (background_port) {
-        background_port.postMessage({action: 'abort'});
-    }
+  if (background_port) {
+    background_port.postMessage({action: 'abort'});
+  }
 }
 
 function init() {
-    settings.startMonitoringSettingsStorage();
-    settings.initialiseUi();
-    console.log('init');
-    activateIdle();
-    connectToBackground();
-    registerActionButtons();
-    registerPageButtons();
+  settings.startMonitoringSettingsStorage();
+  settings.initialiseUi();
+  console.log('init');
+  activateIdle();
+  connectToBackground();
+  registerActionButtons();
+  registerPageButtons();
 }
 
 $(document).ready( () => init() );
---
+++ src/js/ui_messages.ts
@@ -0,0 +1,10 @@
+/* Copyright(c) 2023 Philip Mulcahy. */
+
+"use strict";
+
+export const preview_feature_disabled =
+  'This capability is part of a preview feature set that will be ' +
+  'released when charity donations reach a target set out in ' +
+  'https://github.com/philipmulcahy/azad/blob/master/doc/commercial_features.md ' +
+  'BUT you can get early access by subscribing to the preview channel: ' +
+  'click on the "preview features" button to find out more.';
