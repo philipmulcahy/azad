948f07ea33f51fee7ed50f322c5aea96bbbc8056
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2024-12-26 23:17:01 +0000

    while loop, fetching 20 at a time, but not yet parsing

--- src/js/transaction.ts
+++ src/js/transaction.ts
@@ -1,8 +1,15 @@
 import * as extraction from './extraction';
 
+interface Transaction {
+  date: Date,
+  orderId: string,
+  amount: number,
+};
+
 export function initialisePage() {
   if (inIframedTransactionsPage()) {
-    extractPageOfTransactions();
+    const transactions = extractTransactions();
+    console.log(transactions);
   } else {
     plantButton();
   }
@@ -14,13 +21,39 @@ function plantTransactionsIframe() {
   document.body.insertBefore(iframe, document.body.firstChild);
 }
 
-function extractPageOfTransactions() {
-  const transactionElems = extraction.findMultipleNodeValues(
+async function extractTransactions() {
+  const transactions = extractPageOfTransactions();
+  let nextButton = findUsableNextButton();
+  while(nextButton) {
+    nextButton.click();
+    await new Promise(r => setTimeout(r, 5000));
+    const ts = extractPageOfTransactions();
+    console.log('scraped', ts.length, 'transactions');
+    transactions.push(...ts);
+    nextButton = findUsableNextButton();
+  }
+  return transactions;
+}
+
+function extractSingleTransaction(_elem: Element): Transaction | null {
+  return {
+    date: new Date(),
+    orderId: 'XX-XXXXXXXX-XXXXXXX',
+    amount: 9.99,
+  };
+}
+
+function extractPageOfTransactions(): Transaction[] {
+  const transactionElems: Node[] = extraction.findMultipleNodeValues(
     '//div[contains(@class, "apx-transactions-line-item-component-container")]',
     document.documentElement,
     'transaction extraction',
   );
-  console.log('transaction elems', transactionElems);
+
+  return transactionElems
+    .filter(e => e)
+    .map(e => extractSingleTransaction(e as Element))
+    .filter(t => t) as Transaction[];
 }
 
 function inIframedTransactionsPage(): boolean {
@@ -45,4 +78,27 @@ function plantButton() {
     'click',
     _evt => plantTransactionsIframe()
   );
+}
+
+function findUsableNextButton(): HTMLInputElement | null {
+  try {
+    const buttonInputElem = extraction.findSingleNodeValue(
+      '//span[contains(@class, "button")]/span[text()="Next page"]/preceding-sibling::input[not(@disabled)]',
+      document.documentElement,
+      'finding transaction elements'
+    ) as HTMLInputElement;
+    return buttonInputElem;
+  } catch(_) {
+    return null;
+  }
+}
+
+function maybeClickNextPage(): void {
+  const btn = findUsableNextButton();
+  if (btn) {
+    console.log('clicking next page button');
+    btn.click();
+  } else {
+    console.log('no next page button found');
+  }
 }
