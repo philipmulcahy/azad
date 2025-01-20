722fdbfe652f8fbc876378da246f732778a8d633
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2025-01-20 09:44:31 +0000

    remove alltests.bundle.js from package

--- utils/package.sh
+++ utils/package.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 cd $(dirname $0)/..
-npm run build
 rm -f azad.zip
-zip -r azad.zip build
+npm run build
+cp -r build dist
+rm dist/alltests.bundle.js
+zip -r azad.zip dist
+rm -r dist
 
 echo "To test changes, go to chrome://extensions, enable the Developer mode slider, remove the store version of the extension, click 'Load unpacked', and navigate to the build folder."
 echo "If you are the extension owner, go to https://chrome.google.com/webstore/developer/edit/mgkilgclilajckgnedgjgnfdokkgnibi and upload the zip file."
