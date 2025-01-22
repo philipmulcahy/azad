16b3580022e7936742857c95a90b84045f2af35c
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2025-01-22 01:49:14 +0000

    added (commented out) business.ts helpers

---
+++ src/js/business.ts
@@ -0,0 +1,37 @@
+/* Copyright(c) 2025 onwards: Philip Mulcahy. */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Stuff to tackle "Business" accounts (first addressed in 2025).
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import * as urls from '../js/url';
+
+// export function isBusinessAccount(doc: HTMLDocument): boolean {
+
+// }
+
+// export function getBTBGroupKey(): string {
+//   const site = urls.getSite();
+//   const url = urls.normalizeUrl('/gp/css/order-history', site);
+//   return url;
+// }
+
+// export function getOrderHeadersURL(
+//   btbGroupKey: string,
+//   year: number,
+//   startIndex: number
+// ): string {
+//   const site = urls.getSite();
+
+//   return urls.normalizeUrl(
+//     '/gp/your-account/order-history' +
+//     '?opt=ab' +
+//     `&selectedB2BGroupKey=${btbGroupKey}` +
+//     '&digitalOrders=1' +
+//     '&unifiedOrders=1' +
+//     `&orderFilter=year-${year}`,
+//     site
+//   );
+// }
