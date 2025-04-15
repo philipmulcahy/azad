80b5d22ccc45bcdf5513a4accbeb9c3f5d08b6bd
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2025-04-15 17:20:20 +0100

    1) Teach fake_order about amazon's new ways of laying out order headers.

--- src/tests/fake_order.ts
+++ src/tests/fake_order.ts
@@ -88,9 +88,13 @@ export function orderFromTestData(
 
     const list_doc = new jsdom.JSDOM(order_dump.list_html).window.document;
 
+    const order_elems_xpath = `.//*[${['order', 'order-card']
+      .map(x => `contains(concat(" ", normalize-space(@class), " "), "${x} ")`)
+      .join(' or ')}]`
+
     const order_elems = extraction.findMultipleNodeValues(
-        './/*[contains(concat(" ", normalize-space(@class), " "), " order ")]',
-        list_doc.body
+      order_elems_xpath,
+      list_doc.body
     );
 
     const list_elem: HTMLElement = <HTMLElement>(
