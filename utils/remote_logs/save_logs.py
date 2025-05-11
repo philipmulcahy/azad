45f8e6ed1ba70dd5f79eb37e190a767f016d505b
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2025-05-11 22:40:18 +0100

    fix truncation of remote log server and clientip

--- utils/remote_logs/save_logs.py
+++ utils/remote_logs/save_logs.py
@@ -85,13 +85,13 @@ def get_log_lines():
     )
 
     log_line_re = re.compile(
-            r'([^:]*):([a-zA-Z0-9.:]+).*(\d+\.\d+\.\d+\.\d+).*\[(.*) \+\d+\].*(GET|HEAD) ([^ ]+)')
+            r'([^:]*):([a-zA-Z0-9.:-]+).*?(\d+\.\d+\.\d+\.\d+).*?\[(.*) \+\d+\].*?(GET|HEAD) ([^ ]+)')
     #         file
     #                 server
-    #                                   client
-    #                                                           date
-    #                                                                         method
-    #                                                                                    path&params
+    #                                      client
+    #                                                              date
+    #                                                                              method
+    #                                                                                         path&params
 
     def make_log_object(line):
         match = log_line_re.match(line)
