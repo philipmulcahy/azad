2c963e14deb24f8a3a9850104dbcf403c3fd5770
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2025-06-09 01:36:01 +0100

    added (failing) test for DReffects's transactions

--- src/tests/transaction_scrape.test.ts
+++ src/tests/transaction_scrape.test.ts
@@ -1,16 +1,30 @@
-import { extractPageOfTransactions } from '../js/transaction';
+import { extractPageOfTransactions, Transaction } from '../js/transaction';
 const jsdom = require('jsdom');
 import * as fs from 'fs';
+
+function scrapePageOfTransactionsFromCannedHtml(htmlFilePath: string): Transaction[] {
+  const html: string = fs.readFileSync(htmlFilePath, 'utf8');
+  const doc = new jsdom.JSDOM(html).window.document;
+  const transactions = extractPageOfTransactions(doc);
+  return transactions;
+}
 
 describe('can read 20 transactions from first page of philipmulcahy\'s account', () => {
   test(
     'can read 20 transactions', () => {
-      const htmlFilePath = './src/tests/azad_test_data/transactions/philipmulcahy/2025-06-08.html';
-      // const htmlFilePath = './azad_test_data/transactions/philipmulcahy/2025-06-08.html';
-      const html: string = fs.readFileSync(htmlFilePath, 'utf8');
-      const doc = new jsdom.JSDOM(html).window.document;
-      const transactions = extractPageOfTransactions(doc);
+      const transactions = scrapePageOfTransactionsFromCannedHtml(
+        './src/tests/azad_test_data/transactions/philipmulcahy/2025-06-08.html');
+
       expect(transactions.length).toEqual(20);
   });
 });
 
+describe('can read 20 transactions from first page of DReffects\'s account', () => {
+  test(
+    'can read 20 transactions', () => {
+      const transactions = scrapePageOfTransactionsFromCannedHtml(
+        './src/tests/azad_test_data/transactions/DReffects/2025-06-08.html');
+
+      expect(transactions.length).toEqual(20);
+  });
+});
