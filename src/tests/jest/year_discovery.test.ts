eb2bafbb96f510f2e4ae1747e749f65e84a31da8
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2025-06-09 18:33:33 +0100

    close, but no cigar: all tests except for order scraping regression tests now belong to jest, but the order scraping ones blow jest up by logging after jest thinks they're finished - I'm not sure if this is my fault, but I've spent too long investigating, so I tidied them up, and added some more 'npm run verbs' in the package file, to run either test suite or both

---
+++ src/tests/jest/year_discovery.test.ts
@@ -0,0 +1,48 @@
+import * as fs from 'fs';
+const jsdom = require('jsdom');
+import * as periods from '../../js/periods';
+
+function testOneGetYearsTarget(name: string) {
+  const filePath = `./src/tests/azad_test_data/get_years/${name}.html`
+
+  const html_text = fs.readFileSync(filePath, 'utf8');
+  const doc = new jsdom.JSDOM(html_text).window.document;
+  const years: number[] = periods.get_years(doc);
+
+  const result = {
+    test_id: 'GET_YEAR_NUMBERS_' + filePath,
+    years,
+    passed: years.length > 5 && years.length < 25,
+  };
+
+  if (!result.passed) {
+    console.log(result);
+  }
+
+  expect(result.passed).toEqual(true);
+}
+
+describe(
+  'discover available year numbers from several users canned html files',
+  () => {
+    for (const name of [
+     'NathanChristie_2023-09-08',
+     'PhilipMulcahy_2023-09-09',
+     'shood_2023-09-08',
+     'WatersPaul_2025-01-14',
+     'WatersPaul_2025-01-16',
+
+      // Q: why is this test row disabled?
+      // A: the html file doesn't appear to have any four digit year literals
+      //    in it anywhere. The period selector/picker only has 'Last 30 days'
+      //    and 'Last 3 months'.
+      // Q: Why didn't it fail before you migrated it to jest?
+      // A: I don't know.
+      // 'WatersPaul_2025-01-22',
+    ]) {
+      test(name, () => {
+         testOneGetYearsTarget(name);
+      });
+    }
+  }
+);
