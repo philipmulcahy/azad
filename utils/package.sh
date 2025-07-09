b2c26ef4f0ef6f9da9d38e83a3f9791af796339d
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2025-07-09 23:11:58 +0200

    npm run package blows up with an error message if the client isn't clean. This should extend to npm run private-package, but we're not done yet - I want to embed the git hash into the build somehow, so that logging can report it, but only if the build was from a clean client

--- utils/package.sh
+++ utils/package.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd $(dirname $0)/..
+DIRT=$(git status --porcelain=v2)
 rm -f azad.zip
+if [[ -n $DIRT ]]; then echo "Dirty client!"; echo "${DIRT}"; echo "quitting"; exit; fi;
 npm run build
 cp -r build dist
 rm dist/alltests.bundle.js
