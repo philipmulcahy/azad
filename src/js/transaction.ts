5be4403322c1e069203dcd9a8876db4fc37b9adf
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2025-11-12 17:27:36 +0000

    doesn't scroll, and when I help it (by scrolling manuall), up to 85 transactions, it only sees 20 of them

--- src/js/transaction.ts
+++ src/js/transaction.ts
@@ -39,7 +39,9 @@ export async function reallyScrapeAndPublish(
   startDate: Date,
   endDate: Date,
 ) {
-  const transactions = await extractAllTransactions();
+  const transactions = findUsableNextButton() !== undefined ?
+    await extractAllTransactionsWithNextButton() :
+    await extractAllTransactionsWithScrolling();
 
   const filtered = filterTransactionsByDateRange(
     transactions, startDate, endDate);
@@ -127,7 +129,7 @@ function mergeTransactions(
   return ts;
 }
 
-async function extractAllTransactions() {
+async function extractAllTransactionsWithNextButton(): Promise<Transaction[]> {
   let allKnownTransactions = await getTransactionsFromCache();
 
   const maxCachedTimestamp = Math.max(
@@ -144,8 +146,8 @@ async function extractAllTransactions() {
     );
 
     allKnownTransactions = mergeTransactions(page, allKnownTransactions);
-    const nextButton = findUsableNextButton() as HTMLElement;
-    const nextButtonFound = nextButton !== undefined && nextButton !== null;
+    const nextButton = findUsableNextButton();
+    const nextButtonFound = nextButton !== undefined;
     console.debug(`next page button ${nextButtonFound ? '': 'not '}found`);
     const overlappedWithCache = minNewTimestamp < maxCachedTimestamp
                               && allKnownTransactions.length > 0;
@@ -156,11 +158,13 @@ async function extractAllTransactions() {
       );
     }
 
-    shouldContinue = nextButtonFound && !overlappedWithCache;
+    shouldContinue = nextButtonFound &&
+      page.length != 0 &&
+      !overlappedWithCache;
 
     if (shouldContinue) {
       console.log('clicking next page button');
-      nextButton.click();
+      nextButton?.click();
     } else {
       console.log('stopping transaction scrape');
     }
@@ -170,26 +174,122 @@ async function extractAllTransactions() {
   return allKnownTransactions;
 }
 
-function findUsableNextButton(): HTMLInputElement | null {
+async function extractAllTransactionsWithScrolling(): Promise<Transaction[]> {
+  // What behaviour are we exploiting?
+  // ---------------------------------
+  // Scrolling down the transaction list extends the list of transactions
+  // displayed on the page without removing the ones at the top.
+  //
+  // Strategy outline
+  // ----------------
+  // Grow the page until either:
+  // 1) it overlaps with the stuff in the cache,
+  // or:
+  // 2) it stops growing.
+  //    We know it has stopped growing because it has stayed the same size
+  //    after two cycles of scrolling.
+  //
+  // This implies two nested loops - the outer one sends the scroll events
+  // and the inner one observes the transactions repeatedly and decides when
+  // Amazon's scripts have stopped adding more.
+
+  const cachedTransactions = await getTransactionsFromCache();
+
+  const maxCachedTimestamp = Math.max(
+    ...cachedTransactions.map(t => t.date.getTime())
+  );
+
+  // transaction counts from each call to getTransactionsFromPageAfterScroll()
+  const counts: number[] = [];
+
+  // latest and greatest take from the page we're on
+  let page: Transaction[] = [];
+
+  while(!overlapped()) {
+    commandScroll();
+    page = await getTransactionsFromPage();
+    counts.push(page.length);
+  }
+
+  const mergedTransactions = mergeTransactions(page, cachedTransactions);
+  putTransactionsInCache(mergedTransactions);
+  return mergedTransactions;
+
+  function overlapped(): boolean {
+    return page.map(t => t.date.getTime())
+               .some(d => d < maxCachedTimestamp);
+  }
+
+  // Has scrolling stopped giving us more transactions?
+  function theWellIsDry(): boolean {
+    if (counts.length <= 1) {
+      return false;  // We don't know, because we've not tried hard enough yet.
+    }
+
+    return counts.at(-1) == counts.at(-3);  // compare last with third last.
+  }
+
+  function commandScroll(): void {
+    console.log('scrolling down by one page');
+
+    const evt = new WheelEvent("wheel", {
+      deltaMode: WheelEvent.DOM_DELTA_PAGE,
+      deltaY: -100,
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    });
+
+    const elem = findScrollableElem();
+    elem?.dispatchEvent(evt);
+  }
+
+  function findScrollableElem(): HTMLElement | undefined {
+    return (
+      document.querySelector(
+        'a[data-testid="transaction-link"]'
+      ) as HTMLElement
+    ) ?? undefined;
+  }
+
+  async function getTransactionsFromPage(): Promise<Transaction[]> {
+    let elapsedMillis: number = 0;
+    const DEADLINE_MILLIS = 10 * 1000;
+    const INCREMENT_MILLIS = 1000;
+
+    while (elapsedMillis <= DEADLINE_MILLIS) {
+      console.log('waiting', INCREMENT_MILLIS);
+      await new Promise(r => setTimeout(r, INCREMENT_MILLIS));
+      elapsedMillis += INCREMENT_MILLIS;
+      console.log('elapsedMillis', elapsedMillis);
+      const page = extractPageOfTransactions(document);
+      console.log(`got ${page.length} transactions`);
+
+      if (page.length > 0) {
+        // Wait before trying one final time in case amazon code was still
+        // running when we sampled.
+        console.log(`waiting (once) before scraping one last time`);
+        await new Promise(r => setTimeout(r, INCREMENT_MILLIS));
+        const finalTry = extractPageOfTransactions(document);
+        console.log(`got ${finalTry.length} transactions`);
+        return finalTry;
+      }
+    }
+
+    return [];
+  }
+}
+
+function findUsableNextButton(): HTMLInputElement | undefined {
   try {
     const buttonInputElem = extraction.findSingleNodeValue(
       '//span[contains(@class, "button")]/span[text()="Next page" or text()="Next Page"]/preceding-sibling::input[not(@disabled)]',
       document.documentElement,
       'finding transaction elements'
     ) as HTMLInputElement;
-    return buttonInputElem;
+    return buttonInputElem ?? undefined;
   } catch(_) {
-    return null;
-  }
-}
-
-function maybeClickNextPage(): void {
-  const btn = findUsableNextButton();
-  if (btn) {
-    console.log('clicking next page button');
-    btn.click();
-  } else {
-    console.log('no next page button found');
+    return undefined;
   }
 }
 
