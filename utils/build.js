eb207382621d99307f2ea9ca233bc016285550b1
PHILIP MULCAHY <1668892+philipmulcahy@users.noreply.github.com>
2026-06-29 23:47:20 +0200

    fixed build.js

--- utils/build.js
+++ utils/build.js
@@ -1,6 +1,8 @@
 /* Copyright(c) 2025 Philip Mulcahy. */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const webpack = require("webpack");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require("../webpack.config");
 
 delete config.chromeExtensionBoilerplate;
